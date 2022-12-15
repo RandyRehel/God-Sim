b3f71bac7b771f087b57e190a7caea092d6e8bbe
Randy Rehel <smartiesgoldgaming@gmail.com>
2022-12-15 18:02:36 -0500

    biiiiiiiiiig advancements

--- Assets/Scripts/MenuItemRespawn.cs
+++ Assets/Scripts/MenuItemRespawn.cs
@@ -7,10 +7,10 @@ using UnityEngine.XR.Interaction.Toolkit;
 
 public class MenuItemRespawn : XRSocketInteractor
 {
-    bool cdDone = true;
-    
+    public GameObject listRef;
     // Update is called once per frame
     
+
     protected override void OnSelectExiting(SelectExitEventArgs args)
     {
         base.OnSelectExiting(args);
@@ -23,9 +23,13 @@ public class MenuItemRespawn : XRSocketInteractor
 
     public IEnumerator RespawnObj(IXRSelectInteractable obj, XRSocketInteractor interactor)
     {
-
+            
             GameObject cloneObj = obj.transform.gameObject;
+            cloneObj.transform.parent = null;
             GameObject refClone = Instantiate(cloneObj, interactor.attachTransform.position,interactor.attachTransform.rotation);
+            listRef.GetComponent<InputManager>().listObjets.Add(refClone);
+            listRef.GetComponent<InputManager>().socket.Add(interactor.attachTransform.transform.gameObject);
+            refClone.transform.parent = null;
             refClone.GetComponent<Rigidbody>().isKinematic = false;
             refClone.GetComponent<Rigidbody>().useGravity = true;
             yield return null;
